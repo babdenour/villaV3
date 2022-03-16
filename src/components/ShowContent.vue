3716c68ee71fbeab5a5bfb6e719a16bd47b86baa
abdenour <abdenourbensouna@gmail.com>
2022-03-16 20:26:59 +0100

    show dataset

--- src/components/ShowContent.vue
+++ src/components/ShowContent.vue
@@ -1,10 +1,17 @@
 <template>
-  <div :id="time" class="img_desc" @click="swipeTime()">
+  <div
+    class="img_desc"
+    :data-time="time"
+    :data-name="name"
+    :data-path="path"
+    :data-fL="floorLocation"
+    @click="swipeTime()"
+  >
     <img :alt="name" :src="require(`@/${path}`)" />
-    <div class="img_desc__desc stack" style="--stacks: 3;">
-      <span style="--index: 0;">{{ desc }} {{ name }} {{ time }}</span>
-      <span style="--index: 1;">{{ desc }} {{ name }} {{ time }}</span>
-      <span style="--index: 2;">{{ desc }} {{ name }} {{ time }}</span>
+    <div class="img_desc__desc stack" style="--stacks: 3">
+      <span style="--index: 0">{{ desc }} {{ name }} {{ time }}</span>
+      <span style="--index: 1">{{ desc }} {{ name }} {{ time }}</span>
+      <span style="--index: 2">{{ desc }} {{ name }} {{ time }}</span>
     </div>
   </div>
 </template>
@@ -21,65 +28,74 @@
   display: flex;
   flex-direction: column;
 
-.stack {
-  display: grid;
-  grid-template-columns: 1fr;
-  margin-bottom: 0.5rem;
-}
+  .stack {
+    display: grid;
+    grid-template-columns: 1fr;
+    margin-bottom: 0.5rem;
+  }
 
-.stack span {
-  font-weight: bold;
-  grid-row-start: 1;
-  grid-column-start: 1;
-  font-size: 16px;
-  --stack-height: calc(100% / var(--stacks) - 1px);
-  --inverse-index: calc(calc(var(--stacks) - 1) - var(--index));
-  --clip-top: calc(var(--stack-height) * var(--index));
-  --clip-bottom: calc(var(--stack-height) * var(--inverse-index));
-  clip-path: inset(var(--clip-top) 0 var(--clip-bottom) 0);
-  animation: stack 140ms cubic-bezier(.46,.29,0,1.24) 1 backwards calc(var(--index) * 10ms), glitch 3s ease infinite 3s alternate-reverse;
-}
+  .stack span {
+    font-weight: bold;
+    grid-row-start: 1;
+    grid-column-start: 1;
+    font-size: 16px;
+    --stack-height: calc(100% / var(--stacks) - 1px);
+    --inverse-index: calc(calc(var(--stacks) - 1) - var(--index));
+    --clip-top: calc(var(--stack-height) * var(--index));
+    --clip-bottom: calc(var(--stack-height) * var(--inverse-index));
+    clip-path: inset(var(--clip-top) 0 var(--clip-bottom) 0);
+    animation: stack 140ms cubic-bezier(0.46, 0.29, 0, 1.24) 1 backwards
+        calc(var(--index) * 10ms),
+      glitch 3s ease infinite 3s alternate-reverse;
+  }
 
-.stack span:nth-child(odd) { --glitch-translate: 8px; }
-.stack span:nth-child(even) { --glitch-translate: -8px; }
+  .stack span:nth-child(odd) {
+    --glitch-translate: 8px;
+  }
+  .stack span:nth-child(even) {
+    --glitch-translate: -8px;
+  }
 
-@keyframes stack {
-  0% {
-    opacity: 0;
-    transform: translateX(-50%);
-    text-shadow: -2px 3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
-  };
-  60% {
-    opacity: 0.5;
-    transform: translateX(50%);
+  @keyframes stack {
+    0% {
+      opacity: 0;
+      transform: translateX(-50%);
+      text-shadow: -2px 3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
+    }
+    60% {
+      opacity: 0.5;
+      transform: translateX(50%);
+    }
+    80% {
+      transform: none;
+      opacity: 1;
+      text-shadow: 2px -3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
+    }
+    100% {
+      text-shadow: none;
+    }
   }
-  80% {
-    transform: none;
-    opacity: 1;
-    text-shadow: 2px -3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
-  }
-  100% {
-    text-shadow: none;
-  }
-}
 
-@keyframes glitch {
-  0% {
-    text-shadow: -2px 3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
-    transform: translate(var(--glitch-translate));
+  @keyframes glitch {
+    0% {
+      text-shadow: -2px 3px 0 rgba(250, 250, 0, 0.753), 2px -3px 0 rgba(0, 0, 255, 0.753);
+      transform: translate(var(--glitch-translate));
+    }
+    2% {
+      text-shadow: 2px -3px 0 rgba(250, 250, 0, 0.753), -2px 3px 0 rgba(0, 0, 255, 0.753);
+    }
+    4%,
+    100% {
+      text-shadow: none;
+      transform: none;
+    }
   }
-  2% {
-    text-shadow: 2px -3px 0 rgba(250, 250, 0, 0.753), -2px 3px 0 rgba(0, 0, 255, 0.753);
-  }
-  4%, 100% {  text-shadow: none; transform: none; }
-}
 }
 </style>
 
 <script>
-
 export default {
-  name: 'ShowContent',
+  name: "ShowContent",
   props: {
     name: String,
     path: String,
@@ -89,7 +105,7 @@ export default {
   },
   methods: {
     swipeTime: () => {
-      console.log('click swipeTime fct');
+      console.log("click swipeTime fct");
     },
   },
 };
